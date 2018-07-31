44a0cdbba25cec8ef882a328c74ab6958c05aa76
vunguyen <you@example.com>
2018-07-31 23:44:40 +0700

    Fix UI

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import {
   Text,
   View,
   Image,
-  ImageBackground
+  ImageBackground,
+  FlatList
 } from 'react-native';
 import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view'
 import { width, height } from 'react-native-dimension'
-import ImageSequence from 'react-native-image-sequence'
 import Carousel from 'react-native-snap-carousel';
+import store from './config/store'
 const images = [
   require('./images/images1.jpeg'),
   require('./images/images2.jpeg'),
@@ -31,20 +32,53 @@ class HelloWorld extends Component {
   _renderItem({ item, index }) {
     return (
       <View style={styles.slider}>
-        <Image source={item} style={{ width : width(100), height : height(18)}} resizeMode={'cover'}/>
+        <Image source={item} style={{ width: width(100), height: height(23) }} resizeMode={'cover'} />
       </View>
     );
+  }
+  componentWillMount() {
+    console.log('store', store)
+  }
+
+  renderStoreItem = ({ item }) => {
+    return (
+      <View style={{
+        width: width(25), height: width(25), backgroundColor: 'white', margin: height(2), alignItems: 'center', justifyContent: 'center', shadowColor: "black", alignSelf: 'center',
+        shadowOffset: { height: height(1) },
+        shadowRadius: height(0.5),
+        borderRadius: height(1),
+        elevation: 5,
+        shadowOpacity: 0.5
+      }}>
+        <Image source={item.linkImage} style={{ width: width(25), height: width(25), alignSelf: 'center', borderRadius: height(1) }} resizeMode='contain' />
+      </View>
+    )
+  }
+  // TODO check number image  depend on width
+  renderStoreData = () => {
+    return (
+      <View style={{ marginBottom: height(5) }}>
+        <FlatList
+          data={store}
+          contentInsetAdjustmentBehavior={'automatic'}
+          contentContainerStyle={{ backgroundColor: 'transparent', alignItems: 'center', justifyContent: 'center' }}
+          numColumns={3}
+          renderItem={this.renderStoreItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    )
   }
 
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground
-          source={require('./images/background4.jpg')}
+          source={require('./images/background8.jpg')}
           style={styles.imgBackground}
         >
-      <View style={{ height : height(20) }}>
-           <Carousel
+          <View style={{ height: height(25) }}>
+            <Carousel
               ref={(c) => { this._carousel = c; }}
               data={images}
               renderItem={this._renderItem}
@@ -55,28 +89,25 @@ class HelloWorld extends Component {
               autoplayInterval={3000}
               inactiveSlideScale={0.94}
               inactiveSlideOpacity={0.7}
-            // inactiveSlideShift={20}
-            containerCustomStyle={styles.slider}
-            contentContainerStyle={styles.container}
-            contentContainerCustomStyle={styles.sliderContentContainer}
-            loop={true}
-            loopClonesPerSide={1}
-            autoplay={true}
-            autoplayDelay={500}
-            autoplayInterval={3000}
+              containerCustomStyle={styles.slider}
+              contentContainerStyle={styles.container}
+              contentContainerCustomStyle={styles.sliderContentContainer}
+              loop={true}
+              loopClonesPerSide={1}
+              autoplayDelay={500}
             />
 
-      </View>
-        <ScrollableTabView
-          initialPage={0}
-          renderTabBar={() => <DefaultTabBar />}>
-          <View tabLabel='COUPONS'>
-          <Text>{'COUPONS'}</Text>
           </View>
-          <View tabLabel='STORES'>
-            <Text>{'STORES'}</Text>
-          </View>
-        </ScrollableTabView>
+          <ScrollableTabView
+            initialPage={0}
+            renderTabBar={() => <DefaultTabBar />}>
+            <View tabLabel='COUPONS'>
+              <Text>{'COUPONS'}</Text>
+            </View>
+            <View tabLabel='STORES'>
+              {this.renderStoreData()}
+            </View>
+          </ScrollableTabView>
         </ImageBackground>
       </View>
     );
@@ -130,9 +161,15 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   slider: {
-    paddingTop : height(1),
+    paddingTop: height(1),
     overflow: 'visible', // for custom animations
-    
+    shadowColor: "transparent", alignSelf: 'center',
+    shadowOffset: { height: height(1) },
+    shadowRadius: height(0.5),
+    borderRadius: height(1),
+    elevation: 5,
+    shadowOpacity: 0.5
+
   },
   sliderContentContainer: {
     paddingVertical: 10 // for custom animation
@@ -141,7 +178,27 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     flex: 1
-  }
+  },
+  containerStoreData: {
+    // flex: 1,
+    alignItems: 'center',
+    // alignContent: 'center',
+    //flexDirection: 'row',
+    // flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  touchButton: {
+    alignSelf: 'center',
+    backgroundColor: '#2980b9',
+    paddingVertical: 25,
+    width: 295,
+    margin: 15,
+  },
+  touchButtonText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold'
+  },
 });
 
 export default HelloWorld
---
+++ config/store.js
@@ -0,0 +1,123 @@
+const store = [
+    {
+        "title": 'Amazon Coupons',
+        "link": 'https://www.amazon.com/Coupons/',
+        "linkImage" : require('../images/stores/amazon.jpeg')
+    },
+    {
+        "title": 'CashKaro Coupons',
+        "link": 'https://cashkaro.com/',
+        "linkImage": require('../images/stores/cashkaro.jpeg')
+    },
+    {
+        "title": 'Coupon Dunia',
+        "link": 'https://www.coupondunia.in/',
+        "linkImage": require('../images/stores/coupondunia.png')
+    },
+    {
+        "title": 'Coupons',
+        "link": 'https://www.coupons.com/',
+        "linkImage": require('../images/stores/coupons.png')
+    },
+    {
+        "title": 'Couponz Guru',
+        "link": 'https://www.couponzguru.com/',
+        "linkImage": require('../images/stores/couponzguru.png')
+    },
+    {
+        "title": 'Deal Catcher',
+        "link": 'https://www.dealcatcher.com/',
+        "linkImage": require('../images/stores/dealcatcher.jpeg')
+    },
+    {
+        "title": 'Deal News',
+        "link": 'https://www.dealnews.com/',
+        "linkImage": require('../images/stores/dealnews.png')
+    },
+    {
+        "title": 'Ebates Coupons',
+        "link": 'https://www.ebates.com/',
+        "linkImage": require('../images/stores/ebates.jpeg')
+    },
+    {
+        "title": 'FreeShipping Coupons',
+        "link": 'http://www.freeshipping.org/',
+        "linkImage": require('../images/stores/freeshipping.jpeg')
+    },
+    {
+        "title": 'Free Shopping Coupons',
+        "link": 'http://freeshopping.co/',
+        "linkImage": require('../images/stores/freeshopping.png')
+    },
+    {
+        "title": 'Frugaa Coupons',
+        "link": 'https://www.frugaa.com/',
+        "linkImage": require('../images/stores/frugaa.png')
+    },
+    {
+        "title": 'Grocery Coupons',
+        "link": 'https://www.grocerycouponnetwork.com/coupons/',
+        "linkImage": require('../images/stores/grocerycoupon.jpeg')
+    },
+    {
+        "title": 'Groupon Coupons',
+        "link": 'https://www.groupon.com/',
+        "linkImage": require('../images/stores/groupon.jpeg')
+    },
+    {
+        "title": 'Krazy Counpon Lady',
+        "link": 'https://thekrazycouponlady.com/print-coupons',
+        "linkImage": require('../images/stores/krazycouponlady.png')
+    },
+    {
+        "title": 'Living Social Coupon',
+        "link": 'https://www.livingsocial.com/',
+        "linkImage": require('../images/stores/livingsocial.jpeg')
+    },
+    {
+        "title": 'Money Saving Mom',
+        "link": 'https://moneysavingmom.com/',
+        "linkImage": require('../images/stores/moneysavingmom.png')
+    },
+    {
+        "title": 'PayBack Coupon',
+        "link": 'https://www.payback.in/',
+        "linkImage": require('../images/stores/payback.jpeg')
+    },
+    {
+        "title": 'RetailMeNot Coupon',
+        "link": 'https://www.retailmenot.com/',
+        "linkImage": require('../images/stores/retailmenot.jpeg')
+    },
+    {
+        "title": 'Saving Coupon',
+        "link": 'https://www.savings.com/',
+        "linkImage": require('../images/stores/saving.jpeg')
+    },
+    {
+        "title": 'SheSaved Coupon',
+        "link": 'https://www.shesaved.com/',
+        "linkImage": require('../images/stores/shesaved.com.png')
+    },
+    {
+        "title": 'Shop At Home Coupon',
+        "link": 'https://www.shopathome.com/',
+        "linkImage": require('../images/stores/shopathome.png')
+    },
+    {
+        "title": 'Slick Deals Coupon',
+        "link": 'https://slickdeals.net/',
+        "linkImage": require('../images/stores/slickdeals.png')
+    },
+    {
+        "title": 'Smart Source Coupon',
+        "link": 'http://www.smartsource.com/',
+        "linkImage": require('../images/stores/smartsource.png')
+    },
+    {
+        "title": 'Woot Coupon',
+        "link": 'https://www.woot.com/',
+        "linkImage": require('../images/stores/woot.png')
+    }
+]
+export default store
